6af92144893c6155867490d8d718bc21ba2d409e
Ariell SCott <scott.ariell00@gmail.com>
2021-11-01 19:27:31 -0400

    completed category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,8 +4,12 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  const productData = await
+  Product.findAll({
+    include: [{model: Category}]
+  });
   // be sure to include its associated Category and Tag data
 });
 
